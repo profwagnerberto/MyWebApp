20aae15c16fec98abd4556e0bd70b95bfb8743b7
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-07-04 09:46:17 -0300

    Result message coded.

--- src/main/java/com/mycompany/UserController.java
+++ src/main/java/com/mycompany/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
@@ -32,9 +33,11 @@ public class UserController {
   }
   
   @PostMapping("/users/save")
-  public String saveUser(User user) {
+  public String saveUser(User user, RedirectAttributes ra) {
     
     service.save(user);
+    
+    ra.addFlashAttribute("message", "The user has been saved successfully.");
     
     return "redirect:/users";    
   }
